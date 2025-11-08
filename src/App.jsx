15d9e20732c1220676b3d5c92ac054d278c7d2ef
caraven <caraven90@gmail.com>
2025-11-08 09:21:58 -0500

    ejercicio temrinado

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+//import FetchData from './Components/FetchData'
+import FetchYogaData from './Components/FecthYogaData'
 import FetchData from './Components/FetchData'
 
 function App() {
   return (
  <>
- <FetchData/>
+ {/* <FetchData/> */}
+ <FetchYogaData/>
  </>
   )
 }
--- src/Components/FecthYogaData.jsx
+++ src/Components/FecthYogaData.jsx
@@ -1,21 +1,19 @@
-import React from 'react'
+import React from 'react';
 import UseFetch from './UseFetch';
-import './FetchData.css';
 
-const FetchData = () => {
-    const [data] = UseFetch('https://api.npoint.io/9045c260b1565daa9e15');
+const FetchYogaData = () => {
+    const [data] = UseFetch('https://api.npoint.io/4459a9a10e43812e1152');
     console.log(data);
     return (
         <>
             <ul className='list_data_main'>
-                <h1 className='usefetch_heading'>Use Fetch Custom Hook</h1>
+                <h1 className='usefetch_heading'>Beneficio del Yoga</h1>
                 {
                     data && data.map((e) => (
                         <li className='list_data'>
                             <h3>{e.name}</h3>
-                            <p><strong>Importancia: </strong>{e.importance}</p>
                             <p><strong>Beneficios: </strong>{e.benefits}</p>
-                            <p><strong>Hora para comer: </strong>{e.best_time_to_intake}</p>
+                            <p><strong>Duración: </strong>{e.time_duration}</p>
                         </li>
                     ))
                 }
@@ -23,5 +21,4 @@ const FetchData = () => {
         </>
     )
 }
-
-export default FetchData
+export default FetchYogaData
